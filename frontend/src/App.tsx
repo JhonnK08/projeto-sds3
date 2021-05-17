0d21329c2eadee9606fe857a86cc9c255c96cb48
Jhonatan <38366068+JhonnK08@users.noreply.github.com>
2021-05-17 00:41:30 -0300

    fix types

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,3 @@
-import Navbar from "components/Navbar";
-import Footer from "components/Footer";
-import DataTable from "components/DataTable";
-import BarChart from "components/BarChart";
-import DonutChart from "components/DonutChart";
 import Routes from "Routes";
 
 function App() {
